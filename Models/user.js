45731c5ecbefdaaa41bec6757b93df2cb7b4f228
hanasiu <102237333+hanasiu@users.noreply.github.com>
2022-09-28 00:04:53 +0900

    Add files via upload

---
+++ Models/user.js
@@ -0,0 +1,58 @@
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost:27017/relationshipDemo', { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+        console.log("Mongo Connection Open!!");
+    })
+    .catch(err => {
+        console.log("Oh No Mongo Connection Error!!");
+        console.log(err);
+    })
+
+
+const userSchema = new mongoose.Schema({
+    first: String,
+    last: String,
+    addresses: [
+        {
+            _id: { id: false },
+            street: String,
+            city: String,
+            state: String,
+            country: String
+        }
+    ]
+})
+
+const User = mongoose.model('User', userSchema);
+
+const makeUser = async () => {
+    const u = new User({
+        first: 'Harry',
+        last: 'Potter'
+    })
+    u.addresses.push({
+        street: '123 Sesame St.',
+        city: 'New York',
+        state: 'NY',
+        country: 'USA'
+    })
+    const res = await u.save();
+    console.log(res);
+}
+
+const addAddress = async (id) => {
+    const user = await User.findById(id);
+    user.addresses.push(
+        {
+            street: '99 3rd St.',
+            city: 'New York',
+            state: 'NY',
+            country: 'USA'
+        }
+    )
+    const res = await user.save();
+    console.log(res);
+}
+
+addAddress('6332bb16222de4aa83ff88bf');
